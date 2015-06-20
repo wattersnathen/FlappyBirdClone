6a0686a5c67e5c2db2393e1adb1419d601a23d4a
Nathen <wattersnathen@gmail.com>
2015-06-20 12:41:58 -0600

    initial gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+
+var browserify = require('browserify');
+var sourceStream = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+gulp.task('js', function () {
+    return browserify('./js/main.js')
+        .bundle()
+        .pipe(sourceStream('app.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('./js'));
+});
+    
+gulp.task('watch', function () {
+    gulp.watch(['./js/**/*.js', '!./js/app.js'], ['js']);
+});
+    
+gulp.task('default', ['js', 'watch']);

2b023f5f330c4906c1d6287d4bf0c8348bbb0018
Nathen <wattersnathen@gmail.com>
2015-06-26 05:53:22 -0600

    switched pipe to pipesystem

--- js/flappybird.js
+++ js/flappybird.js
@@ -1,20 +1,23 @@
 var graphicsSystem = require('./systems/graphics');
 var physicsSystem = require('./systems/physics');
 var inputSystem = require('./systems/input');
+var pipeSystem = require('./systems/pipe');
+
 var bird = require('./entities/bird');
-var pipe = require('./entities/pipe');
 
 var FlappyBird = function () {
-    this.entities = [new bird.Bird(), new pipe.Pipe()];
+    this.entities = [new bird.Bird()];
     this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
     this.physics = new physicsSystem.PhysicsSystem(this.entities);
     this.input = new inputSystem.InputSystem(this.entities);
+    this.pipes = new pipeSystem.PipeSystem(this.entities);
 };
 
 FlappyBird.prototype.run = function () {
     this.graphics.run();
     this.physics.run();
     this.input.run();
+    this.pipes.run();
 };
 
 exports.FlappyBird = FlappyBird;

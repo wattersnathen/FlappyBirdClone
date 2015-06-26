41639b8598bf2b7770992c7a00503260022eab3a
Nathen <wattersnathen@gmail.com>
2015-06-26 05:50:39 -0600

    Adding settings file for reusable values

---
+++ js/settings.js
@@ -0,0 +1,6 @@
+/* 
+    Using a settings.js file to store common property 
+    values throughoutthe application.
+*/
+exports.pipeWidth = 0.15;   // rectangular width of the pipes to be the same
+exports.pipeGap = 0.2;      // distance between the pipes

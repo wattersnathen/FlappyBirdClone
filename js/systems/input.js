0d19f3e90c15657e8575201089c4c3db7d7808e6
Nathen <wattersnathen@gmail.com>
2015-06-26 05:52:47 -0600

    formatting change

--- js/systems/input.js
+++ js/systems/input.js
@@ -1,15 +1,15 @@
-var InputSystem = function(entities) {
+var InputSystem = function (entities) {
     this.entities = entities;
 
     // Canvas is where we get input from
     this.canvas = document.getElementById('canvas');
 };
 
-InputSystem.prototype.run = function() {
+InputSystem.prototype.run = function () {
     this.canvas.addEventListener('click', this.onClick.bind(this));
 };
 
-InputSystem.prototype.onClick = function() {
+InputSystem.prototype.onClick = function () {
     var bird = this.entities[0];
     bird.components.physics.velocity.y = 0.7;
 };

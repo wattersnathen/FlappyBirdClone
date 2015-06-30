34a04761ddcd6d4fc447c8e27e869045fcd13d3d
Nathen Watters <wattersnathen@gmail.com>
2015-06-29 22:24:45 -0600

    Attaching app to window object for debugging purposes

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ var flappyBird = require('./flappybird');
 
 document.addEventListener('DOMContentLoaded', function () {
     var app = new flappyBird.FlappyBird();
+    window.app = app;
     app.run();
 });

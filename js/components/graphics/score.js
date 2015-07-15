c54edf004c5efaaedcae682b1f1a5f5e1bb1bafa
Nathen Watters <wattersnathen@gmail.com>
2015-07-15 06:33:38 -0600

    attempt at hiding the score entities

--- js/components/graphics/score.js
+++ js/components/graphics/score.js
@@ -8,7 +8,7 @@ ScoreGraphicsComponent.prototype.draw = function (context) {
 
 	context.save();
 	context.translate(position.x, position.y);
-	context.fillStyle = "#FF00FF";
+	context.fillStyle = "rgba(0, 0, 0, 0)";
 	context.beginPath();
 	context.rect(-this.size.x / 2, -this.size.y / 2, this.size.x, this.size.y);
 	context.fill();

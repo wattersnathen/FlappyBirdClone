1d58ea755c7022a1101ec4073ee5eee5664cb0a1
Nathen Watters <wattersnathen@gmail.com>
2015-07-18 13:04:19 -0600

    Created system for garbage collection, Moved previous entity destroy logic from pipe system to new collector system, updated physics system to include and run new collector system

---
+++ js/systems/collector.js
@@ -0,0 +1,45 @@
+var leftedge = require('../entities/leftedge');
+var rightedge = require('../entities/rightedge');
+var topedge = require('../entities/topedge');
+var bottomedge = require('../entities/bottomedge');
+
+var pipe = require('../entities/pipe');
+var score = require('../entities/score');
+
+var shuttle = require('../entities/shuttle');
+
+var CollectorSystem = function (entities) {
+	this.entities = entities;
+	this.paused = false;
+
+	this.canvas = document.getElementById('canvas');
+	this.canvasAspectRatio = this.canvas.width / this.canvas.height;
+};
+
+CollectorSystem.prototype.tick = function () {
+
+	for (var idx = 0, len = this.entities.length; idx < len; idx++) {
+		// reference to current entity
+		var entity = this.entities[idx];
+
+		// collect pipes and score objects
+	    if ((entity instanceof pipe.Pipe || entity instanceof score.Score)
+	        && entity.components.physics.position.x < -this.canvasAspectRatio / 2) {
+	    	console.log('pipe off screen');
+	        this.entities.splice(idx, 1);
+	        idx--;
+	    }
+
+	    // collect shuttles
+	    if (entity instanceof shuttle.Shuttle && ( 
+	    	entity.components.physics.position.x > this.canvasAspectRatio / 2 + 0.1 ||
+	    	entity.components.physics.position.y > 1 ||
+	    	entity.components.physics.position.y < 0)) {
+	    	this.entities.splice(idx, 1);
+	    	idx--;
+	    }
+	}	
+
+};
+
+exports.CollectorSystem = CollectorSystem;

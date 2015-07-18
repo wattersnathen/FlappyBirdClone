c1c5fb1a4db383cf563dc648e98613d9fdfd4070
Nathen Watters <wattersnathen@gmail.com>
2015-07-18 13:07:05 -0600

    increased pipe gap slightly

--- js/settings.js
+++ js/settings.js
@@ -3,7 +3,7 @@
     values throughoutthe application.
 */
 exports.pipeWidth = 0.15;   	// rectangular width of the pipes to be the same
-exports.pipeGap = ((location.hash == '#cheat') ? 0.7 : 0.2); 	// distance between the pipes
+exports.pipeGap = ((location.hash == '#cheat') ? 0.7 : 0.225); 	// distance between the pipes
 exports.birdWidth = 0.02;		// width of the bird object
 exports.birdStartPos = {		// starting position of the bird
 	x: 0,

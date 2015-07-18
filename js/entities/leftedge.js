efa5f9b1b3f1d51441bef5cde8c20dc8d14af11d
Nathen Watters <wattersnathen@gmail.com>
2015-07-18 13:06:42 -0600

    added left and right edge entities

---
+++ js/entities/leftedge.js
@@ -0,0 +1,33 @@
+var physicsComponent = require('../components/physics/physics');
+var collisionComponent = require('../components/collision/rect');
+var graphicsComponent = require('../components/graphics/pipe');
+
+var settings = require('../settings');
+
+var pipe = require('./pipe');
+var score = require('./score');
+
+var LeftEdge = function () {
+	this.dimens = {
+		x: 0.001,
+		y: 1     
+	};
+
+	var canvas = document.getElementById('canvas');
+	var aspectRatio = canvas.width / canvas.height;
+
+	var physics = new physicsComponent.PhysicsComponent(this);
+	physics.position.x = -(aspectRatio / 2);
+	physics.position.y = 0.001;
+
+	var graphics = new graphicsComponent.PipeGraphicsComponent(this, this.dimens);
+	var collision = new collisionComponent.RectCollisionComponent(this, this.dimens);
+
+	this.components = {
+		physics: physics,
+		collision: collision,
+		graphics: graphics
+	}
+};
+
+exports.LeftEdge = LeftEdge;
---
+++ js/entities/rightedge.js
@@ -0,0 +1,30 @@
+var physicsComponent = require('../components/physics/physics');
+var collisionComponent = require('../components/collision/rect');
+var graphicsComponent = require('../components/graphics/pipe');
+
+var settings = require('../settings');
+
+var RightEdge = function () {
+	this.dimens = {
+		x: 0.1,
+		y: 1
+	};
+
+	var canvas = document.getElementById('canvas');
+	var aspectRatio = canvas.width / canvas.height;
+
+	var physics = new physicsComponent.PhysicsComponent(this);
+	physics.position.x = aspectRatio / 2;
+	physics.position.y = 0;
+
+	var graphics = new graphicsComponent.PipeGraphicsComponent(this, this.dimens);
+	var collision = new collisionComponent.RectCollisionComponent(this, this.dimens);
+
+	this.components = {
+		physics: physics,
+		collision: collision,
+		graphics: graphics
+	}
+};
+
+exports.RightEdge = RightEdge;
